a9d383edf7f1c85f3b45dca97d536573507b06af
Michael Daugherty <mike@mpdaugherty.com>
2012-03-03 10:04:28 +0000

    Add JSHinting inline

--- config.el
+++ config.el
@@ -5,6 +5,14 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs-config/packages"))
 (add-to-list 'load-path (expand-file-name "~/.emacs-config/modes"))
 
+; When in JS Mode, always run jshint
+(require 'flymake-node-jshint)
+;;; (setq flymake-node-jshint-config "~/.jshintrc-node.json") ; can add config later
+(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
+
+; enhancements for displaying flymake errors with no mouse
+(require 'flymake-cursor)
+
 ; Add Major modes
 ;(require 'less-css-mode)
 (load-file "~/.emacs-config/modes/less-css-mode.el")
