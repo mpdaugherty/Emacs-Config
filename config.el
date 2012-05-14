ee5b3a691193246f464085fb62f1f5a2db79a53b
Michael P. Daugherty <mike@mpdaugherty.com>
2012-05-14 17:27:01 +0800

    Adding more modes

--- config.el
+++ config.el
@@ -56,16 +56,53 @@
           (agenda "")))  ; TODO I can set filters, custom view settings, etc. in the next argument after this list.
         ("n" "Next tasks" tags-todo "+next")))
 
-; Add all files in the ~/org/ directory to my agenda
-(setq org-agenda-files (append
-                        (file-expand-wildcards "~/org/*.org")
-                        (file-expand-wildcards "~/Dropbox/LifePhilosophy/*.org")
-                        (delq nil ; this gets us a list of the most recent year, month, and week reviews
-                              (mapcar
-                               (lambda (folder)
-                                 (car (last (sort
-                                       (file-expand-wildcards (concatenate 'string folder "/*.org")) `string-lessp))))
-                              (file-expand-wildcards "~/Dropbox/LifePhilosophy/*Reviews")))))
+; Add all files in the ~/org/ directory and most recent reviews to my agenda
+; Also, make this into an interactive command so I can reload it when I create
+; a new review
+(defun org-reload-agenda-files ()
+  (interactive)
+  (setq org-agenda-files (append
+                          (file-expand-wildcards "~/org/*.org")
+                          (file-expand-wildcards "~/Dropbox/LifePhilosophy/*.org")
+                          (delq nil ; this gets us a list of the most recent year, month, and week reviews
+                                (mapcar
+                                 (lambda (folder)
+                                   (car (last (sort
+                                               (file-expand-wildcards (concatenate 'string folder "/*.org")) `string-lessp))))
+                                 (file-expand-wildcards "~/Dropbox/LifePhilosophy/*Reviews"))))))
+(org-reload-agenda-files)
 
 ; Add SLIME
 (load-file "~/.emacs-config/modes/slime/slime.el")
+
+; Set up my review commands
+(defun create-weekly-review ()
+  (interactive)
+  (with-current-buffer
+   (find-file-noselect "~/Dropbox/LifePhilosophy/WeeklyReviews/abc.org")
+   (org-element-parse-buffer)))
+
+
+;; Add org2blog
+;; Which requires xml-rpx
+(setq load-path (cons "~/.emacs-config/xml-rpc/" (cons "~/.emacs-config/org2blog/" load-path)))
+(require 'xml-rpc)
+(require 'org2blog-autoloads)
+(setq org2blog/wp-blog-alist
+      '(("mpd.com"
+         :url "http://www.mpdaugherty.com/xmlrpc.php"
+         :username "mike")))
+
+;; Add yasnippets
+(add-to-list 'load-path
+              "~/.emacs-config/yasnippet")
+(seq 'yas/snippet-dirs "~/.emacs-config/snippets")
+(require 'yasnippet)
+(yas/global-mode 1)
+
+;; Add django-mode
+(require 'django-html-mode)
+(require 'django-mode)
+(yas/load-directory "~/.emacs-config/modes/django-mode/snippets")
+(add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mode))
+(add-to-list 'auto-mode-alist '("\\.django$" . django-html-mode))
