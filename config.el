881feb20488ab8d219c1c1ecd70f67513890bc98
Michael P. Daugherty <mike@mpdaugherty.com>
2012-10-18 12:26:42 +0800

    Moved README to an org-mode file and add flymake for python.

--- config.el
+++ config.el
@@ -15,6 +15,23 @@
 ; enhancements for displaying flymake errors with no mouse
 (require 'flymake-cursor)
 
+; TODO: Maybe set up some other keys for navigating flymake errors
+;(global-set-key [f10] 'flymake-goto-prev-error)
+;(global-set-key [f11] 'flymake-goto-next-error)
+
+; Add flymake to python
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+               'flymake-create-temp-inplace))
+       (local-file (file-relative-name
+            temp-file
+            (file-name-directory buffer-file-name))))
+      (list "pycheckers"  (list local-file))))
+   (add-to-list 'flymake-allowed-file-name-masks
+             '("\\.py\\'" flymake-pyflakes-init)))
+
 ; Add Major modes
 ;(require 'less-css-mode)
 (load-file "~/.emacs-config/modes/less-css-mode.el")
